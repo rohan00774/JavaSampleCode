27b5c350872d133ebc6e6aed07bddf7747cb38ca
root <root@ip-172-31-5-237.ap-south-1.compute.internal>
2020-05-05 09:54:09 +0000

    Second Commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
- class A {
- public static void main(String args[]){
-     System.out.println("Hello World");
- }
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
 }
